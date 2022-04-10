211a9064ba96cf318abd60afaf1416638ac9cf04
Jingrong Chen <crazyboycjr@gmail.com>
2022-04-10 17:21:13 -0400

    fix cmake issues

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 main() {
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -H. -Bbuild
-    cmake --build build
+    cmake --build build -j
 }
 
 main
--- src/palkia_service/main.cc
+++ src/palkia_service/main.cc
@@ -3,6 +3,7 @@
 
 int main(int argc, char* argv[]) {
   ibv_fork_init();
-  InitGoogleLogging(argc, argv);
+  google::InitGoogleLogging(argv[0]);
+  LOG(INFO) << "Palkia service started";
   // never return;
 }
