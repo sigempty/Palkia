8a6c0e5145b694d7dc39aa3b251b7ab4569a8133
Jingrong Chen <crazyboycjr@gmail.com>
2022-04-12 15:09:34 -0400

    add RemotePtr, implement destructor

--- include/palkia/palkia.h
+++ include/palkia/palkia.h
@@ -2,6 +2,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <assert.h>
+#include <stdio.h>
+
+#include <utility>
 
 #define UNIMPLEMENTED assert(0)
 
@@ -11,6 +14,8 @@ typedef int Result;
 class Connection {
  public:
   Result fetch(void* ptr, ObjectId obj_id, size_t size);
+  Result put(void* ptr, ObjectId obj_id, size_t size);
+  Result invalidate(ObjectId obj_id);
  private:
   struct ibv_qp* qp_;
 };
@@ -45,20 +50,120 @@ struct Metadata {
  * a->b;
 */
 
+inline ObjectId allocate_object_id() {
+  return 0;
+}
+
+template <typename T>
+class RemotePtr {
+ public:
+  template <typename... Args>
+  static RemotePtr<T> allocate_remote(Args &&... args) {
+    auto ptr = RemotePtr<T>::dangling();
+    ptr.metadata = new Metadata();
+    // assign a global unique ID
+    ptr.metadata->obj_id = allocate_object_id();
+    ptr.metadata->flags.cached = true;
+    ptr.val = new T(std::forward(args)...);
+    return std::move(ptr);
+  }
+
+  RemotePtr<T>() { }
+
+  ~RemotePtr<T>() {
+    delete val;
+    val = nullptr;
+    if (metadata) {
+      metadata->conn->invalidate(metadata->obj_id);
+      delete metadata;
+      metadata = nullptr;
+    }
+  }
+
+  RemotePtr<T>(const RemotePtr<T>& other) = delete;
+  RemotePtr<T>& operator=(RemotePtr<T>& other) = delete;
+
+  RemotePtr<T>& operator=(RemotePtr<T>&& other) {
+    val = other.val;
+    metadata = other.metadata;
+    other.val = nullptr;
+    other.metadata = nullptr;
+    return *this;
+  }
+
+  RemotePtr<T>(RemotePtr<T>&& other) {
+    *this = std::move(other);
+  }
+
+
+  inline operator bool() const {
+    return val && metadata;
+  }
+
+  static inline RemotePtr<T> dangling() {
+    auto ptr = RemotePtr<T>();
+    return ptr;
+  }
+
+  // force to swap out to remote memory
+  // if the operation fails, the value should still be there in the local memory
+  inline Result swap_out() {
+    // panic if the pointer is empty
+    assert(this);
+    metadata->flags.cached = false;
+    auto ret = metadata->conn->put(val, metadata->obj_id, sizeof(T));
+    if (ret) {
+      return ret;
+    }
+    // TODO(cjr): delete it lazily
+    delete val;
+    val = nullptr;
+    return 0;
+  }
+
+  // force to do a swap in
+  inline Result swap_in() {
+    // if RDMA
+    return metadata->conn->fetch(&val, metadata->obj_id, sizeof(T));
+    // if local storage, do sth else
+  }
+
+  inline T* get() {
+    if (!metadata->flags.cached) {
+      // fetch the object from the remote memory
+      swap_in();
+    }
+    return val;
+  }
+
+  inline T& operator*() {
+    return *get();
+  }
+
+  inline T* operator->() {
+    return get();
+  }
+
+ private:
+
+  T* val = nullptr;
+  Metadata* metadata = nullptr;
+};
+
+template <typename T, typename... Args>
+RemotePtr<T> make_remote(Args &&... args) {
+  return RemotePtr<T>::allocate_remote(std::forward(args)...);
+}
 
 template <typename T>
 class Remoteable {
  public:
   explicit Remoteable<T>() {
-    metadata = new Metadata();
-    metadata->flags.cached = true;
+    ptr = make_remote<T>();
   }
+
   inline T& deref() {
-    if (!metadata->flags.cached) {
-      // fetch the object from the remote memory
-      metadata->conn->fetch(&val, metadata->obj_id, sizeof(T));
-    }
-    return val;
+    return *ptr;
   }
 
   inline T& operator*() {
@@ -70,6 +175,5 @@ class Remoteable {
   }
 
  private:
-  T val;
-  Metadata* metadata;
+  RemotePtr<T> ptr;
 };
--- src/palkia/palkia.cc
+++ src/palkia/palkia.cc
@@ -3,3 +3,11 @@
 Result Connection::fetch(void* ptr, ObjectId obj_id, size_t size) {
   UNIMPLEMENTED;
 }
+
+Result Connection::put(void* ptr, ObjectId obj_id, size_t size) {
+  UNIMPLEMENTED;
+}
+
+Result Connection::invalidate(ObjectId obj_id) {
+  return 0;
+}
